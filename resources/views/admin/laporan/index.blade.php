2da07d9059bd8bf44801690401da94bc606d46b0
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-09-20 13:53:04 +0700

    repair name karyawan to SDM

--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-12 d-flex justify-content-center">
-                    <h1 class="m-0 text-center">{{ __('Data Laporan') }}</h1>
+                    <h1 class="m-0 text-center">{{ __('Data Laporan Slip Gaji SDM') }}</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
