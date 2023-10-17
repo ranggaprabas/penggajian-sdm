0979786d2aaddd502e756eb9e76aa96e013e5423
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-10-17 17:24:25 +0700

    repair status

--- app/Http/Requests/Admin/UserRequest.php
+++ app/Http/Requests/Admin/UserRequest.php
@@ -45,6 +45,8 @@ class UserRequest extends FormRequest
                         'entitas_id' => 'required',
                         'jenis_kelamin' => 'required',
                         'status' => 'required',
+                        'nama_tunjangan' => 'required|array',
+                        'nilai_tunjangan' => 'required|array',
                     ];
                 }
         }
