95bbc6f4e7347ef4fdd10924f38df2ed0f662aa2
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-11-09 11:31:27 +0700

    repair url web.php absensi

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
         Route::resource('sdm', App\Http\Controllers\Admin\SdmController::class);
         Route::get('users/edit/{id}', [UserController::class, 'edit'])->name('edit-users');
         Route::resource('users', App\Http\Controllers\Admin\UserController::class);
-        Route::get('kehadiran', [App\Http\Controllers\Admin\AbsensiController::class, 'index'])->name('absensis.index');
+        // Route::get('kehadiran', [App\Http\Controllers\Admin\AbsensiController::class, 'index'])->name('absensis.index');
         Route::get('gaji/input-gaji', [App\Http\Controllers\Admin\AbsensiController::class, 'show'])->name('absensis.show');
         Route::post('gaji/input-gaji', [App\Http\Controllers\Admin\AbsensiController::class, 'store'])->name('absensis.store');
         Route::resource('gaji', App\Http\Controllers\Admin\GajiController::class);
@@ -51,8 +51,8 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
         Route::post('laporan/slip-gaji', [App\Http\Controllers\Admin\LaporanController::class, 'store'])->name('laporan.store');
     });
     Route::get('home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('home');
-    Route::get('laporan/slip-gaji/karyawan', [App\Http\Controllers\Admin\LaporanController::class, 'show'])->name('laporan.show');
-    Route::post('laporan/slip-gaji/karyawan', [App\Http\Controllers\Admin\LaporanController::class, 'cekGaji'])->name('laporan.karyawan');
+    // Route::get('laporan/slip-gaji/karyawan', [App\Http\Controllers\Admin\LaporanController::class, 'show'])->name('laporan.show');
+    // Route::post('laporan/slip-gaji/karyawan', [App\Http\Controllers\Admin\LaporanController::class, 'cekGaji'])->name('laporan.karyawan');
 
     Route::get('profile', [\App\Http\Controllers\Admin\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\Admin\ProfileController::class, 'update'])->name('profile.update');
