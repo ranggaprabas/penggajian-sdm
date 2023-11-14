70cbef5f9c542a95a7f2af3ee093e7a7717e6e82
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-11-14 16:39:49 +0700

    repair card color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,7 +97,7 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="card card-info">
+                    <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="card-title">Jumlah SDM berdasarkan Entitas</h3>
 
