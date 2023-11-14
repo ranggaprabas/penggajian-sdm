ada542d4c05a7905c62bcd0b187e34c897fb2782
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-11-14 12:57:34 +0700

    repair title chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -99,7 +99,7 @@
                 <div class="col-md-6">
                     <div class="card card-info">
                         <div class="card-header">
-                            <h3 class="card-title">Data Jumlah SDM Entitas</h3>
+                            <h3 class="card-title">Jumlah SDM berdasarkan Entitas</h3>
 
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
