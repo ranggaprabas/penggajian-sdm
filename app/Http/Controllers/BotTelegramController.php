a28168b8a1021db85df4a2fd98c26a804f3d9d58
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-12-04 13:17:03 +0700

    bot telegram

--- app/Http/Controllers/BotTelegramController.php
+++ app/Http/Controllers/BotTelegramController.php
@@ -10,9 +10,20 @@ class BotTelegramController extends Controller
     //
     public function setWebhook()
     {
-        $response = Telegram::setWebhook(['url'=> env('TELEGRAM_WEBHOOK_URL')]);
-        dd ($response);
+        $response = Telegram::setWebhook(['url' => env('TELEGRAM_WEBHOOK_URL')]);
+
+        if ($response === true) {
+            // Webhook berhasil diatur, tetapi kita ingin menampilkan halaman 404.
+            abort(404);
+        }
+
+        // Jika Anda ingin melakukan sesuatu setelah menetapkan webhook, tambahkan di sini.
+        // ...
+
+        // Respon seharusnya tidak pernah sampai ke sini jika webhook berhasil diatur.
+        dd($response);
     }
+
 
     public function commandHandlerWebhook()
     {
@@ -20,7 +31,7 @@ class BotTelegramController extends Controller
         $chat_id = $updates->getChat()->getId();
         $username = $updates->getChat()->getFirstName();
 
-        if(strtolower($updates->getMessage()->getText() === 'halo')) return Telegram::sendMessage([
+        if (strtolower($updates->getMessage()->getText() === 'halo')) return Telegram::sendMessage([
             'chat_id' => $chat_id,
             'text' => 'Halo ' . $username
         ]);
