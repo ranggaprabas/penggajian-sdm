0254774c04de3094526f64e2815a40c74ad0456d
ranggaprabas <ranggaprabaswara1405@gmail.com>
2023-12-20 11:36:26 +0700

    row new in excel export

--- app/Exports/GajiExport.php
+++ app/Exports/GajiExport.php
@@ -2,7 +2,6 @@
 
 namespace App\Exports;
 
-use App\Absensi;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -18,11 +17,10 @@ class GajiExport implements FromCollection, WithHeadings
 
     public function collection()
     {
-        return $this->items->map(function ($item) {
-            $formattedTunjangan = $this->formatTunjangan($item->tunjangan);
-            $formattedPotongan = $this->formatPotongan($item->potongan);
+        $formattedRows = [];
 
-            return [
+        foreach ($this->items as $item) {
+            $formattedRow = [
                 'id' => $item->id,
                 'sdm_id' => $item->sdm_id,
                 'chat_id' => $item->chat_id,
@@ -33,51 +31,100 @@ class GajiExport implements FromCollection, WithHeadings
                 'nik' => $item->nik,
                 'jenis_kelamin' => $item->jenis_kelamin,
                 'jabatan' => $item->jabatan,
-                'tunjangan' => $formattedTunjangan,
-                'potongan' => $formattedPotongan,
                 'tunjangan_jabatan' => $item->tunjangan_jabatan,
                 'entitas' => $item->entitas,
                 'divisi' => $item->divisi,
             ];
-        });
+
+            // Memisahkan tunjangan menjadi kolom baru
+            $formattedRow += $this->formatTunjangan($item->tunjangan);
+
+            // Memisahkan potongan menjadi kolom baru
+            $formattedRow += $this->formatPotongan($item->potongan);
+
+            $formattedRows[] = $formattedRow;
+        }
+
+        return collect($formattedRows);
     }
 
-    // Metode tambahan untuk memformat tunjangan
     private function formatTunjangan($tunjangan)
     {
         $formattedTunjangan = json_decode($tunjangan, true);
-        $formattedString = '';
-
+        $formattedColumns = [];
+    
         foreach ($formattedTunjangan as $item) {
-            $formattedString .= $item['nama_tunjangan'] . '=' . $item['nilai_tunjangan'] . ', ';
+            $columnName = 'tunjangan_' . $item['nama_tunjangan'];
+            $formattedColumns[$columnName] = $item['nilai_tunjangan'];
         }
-
-        // Menghapus koma dan spasi terakhir
-        $formattedString = rtrim($formattedString, ', ');
-
-        return $formattedString;
+    
+        // Tambahkan kolom tambahan untuk tunjangan yang tidak memiliki nama belakang
+        $tunjanganKeys = array_column($formattedTunjangan, 'nama_tunjangan');
+        $allTunjanganKeys = array_unique(array_merge($this->getAllTunjanganKeys(), $tunjanganKeys));
+    
+        foreach ($allTunjanganKeys as $key) {
+            $columnName = 'tunjangan_' . $key;
+            if (!isset($formattedColumns[$columnName])) {
+                $formattedColumns[$columnName] = null;
+            }
+        }
+    
+        return $formattedColumns;
     }
-
-    // Metode tambahan untuk memformat potongan
+    
     private function formatPotongan($potongan)
     {
         $formattedPotongan = json_decode($potongan, true);
-        $formattedString = '';
-
+        $formattedColumns = [];
+    
         foreach ($formattedPotongan as $item) {
-            $formattedString .= $item['nama_potongan'] . '=' . $item['nilai_potongan'] . ', ';
+            $columnName = 'potongan_' . $item['nama_potongan'];
+            $formattedColumns[$columnName] = $item['nilai_potongan'];
         }
-
-        // Menghapus koma dan spasi terakhir
-        $formattedString = rtrim($formattedString, ', ');
-
-        return $formattedString;
+    
+        // Tambahkan kolom tambahan untuk potongan yang tidak memiliki nama belakang
+        $potonganKeys = array_column($formattedPotongan, 'nama_potongan');
+        $allPotonganKeys = array_unique(array_merge($this->getAllPotonganKeys(), $potonganKeys));
+    
+        foreach ($allPotonganKeys as $key) {
+            $columnName = 'potongan_' . $key;
+            if (!isset($formattedColumns[$columnName])) {
+                $formattedColumns[$columnName] = null;
+            }
+        }
+    
+        return $formattedColumns;
     }
-
-
+    
+    private function getAllTunjanganKeys()
+    {
+        $allKeys = [];
+    
+        foreach ($this->items as $item) {
+            $formattedTunjangan = json_decode($item->tunjangan, true);
+            $tunjanganKeys = array_column($formattedTunjangan, 'nama_tunjangan');
+            $allKeys = array_merge($allKeys, $tunjanganKeys);
+        }
+    
+        return array_unique($allKeys);
+    }
+    
+    private function getAllPotonganKeys()
+    {
+        $allKeys = [];
+    
+        foreach ($this->items as $item) {
+            $formattedPotongan = json_decode($item->potongan, true);
+            $potonganKeys = array_column($formattedPotongan, 'nama_potongan');
+            $allKeys = array_merge($allKeys, $potonganKeys);
+        }
+    
+        return array_unique($allKeys);
+    }
+    
     public function headings(): array
     {
-        return [
+        $headings = [
             'id',
             'sdm_id',
             'chat_id',
@@ -88,11 +135,25 @@ class GajiExport implements FromCollection, WithHeadings
             'nik',
             'jenis_kelamin',
             'jabatan',
-            'tunjangan',
-            'potongan',
             'tunjangan_jabatan',
             'entitas',
             'divisi',
         ];
+    
+        // Dapatkan nama tunjangan dan potongan dari SDM pertama
+        $firstItem = $this->items->first();
+        
+        $tunjanganKeys = [];
+        $potonganKeys = [];
+    
+        if ($firstItem) {
+            $tunjanganKeys = array_keys($this->formatTunjangan($firstItem->tunjangan));
+            $potonganKeys = array_keys($this->formatPotongan($firstItem->potongan));
+        }
+    
+        // Tambahkan nama kolom tunjangan dan potongan
+        $headings = array_merge($headings, $tunjanganKeys, $potonganKeys);
+    
+        return $headings;
     }
-}
+    }
